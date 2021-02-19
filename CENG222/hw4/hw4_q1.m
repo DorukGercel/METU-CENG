f3a5c48e381f69fd74dc1a10d92bc4d8c97bcac9
DorukGercel <69245062+DorukGercel@users.noreply.github.com>
2021-02-19 12:43:48 +0300

    CENG222

---
+++ CENG222/hw4/hw4_q1.m
@@ -0,0 +1,50 @@
+N = 1503; 			%size of Monte Carlo
+lambda = 160; 			%lambda value for Poisson variable
+p = 0.012;			%probability of success
+Total_Triangles = zeros(N,1);		%triangles formed in each simulation
+All_Ratios = zeros(N,1);  %ratio of potential shipments to the total number of triplets 
+
+for k=1:N;			%Each simulation loop
+
+	Poisson_X = poissrnd(lambda);		%No of distinct good types processed within a day
+	Matrix_G = zeros(Poisson_X,Poisson_X);	%Creating the zero matrix according to Poisson_X
+
+	for i=1:Poisson_X;		%Fill the adjacency matrix
+		for j = 1:i-1;
+			U = rand;	%Bernoulli Success Condition
+			if(U < p)		
+				Matrix_G(i,j) = 1;
+			endif;
+ 	
+		endfor;
+	endfor;
+  
+	Matrix_G = Matrix_G + Matrix_G';	%Form the Symmetry Matrix
+
+	No_Triangles = sum(diag(Matrix_G ^ 3))/6;	%No of triangles
+  
+  	No_Triplets = nchoosek(Poisson_X, 3);		%No of triplets
+  
+  	Ratio = No_Triangles/No_Triplets;
+
+	Total_Triangles(k)= No_Triangles;		%No of triangles are saved for this simulation
+  
+  	All_Ratios(k) = Ratio;    %Ratio of triangles to triplets in this simulation
+  
+endfor;
+
+answer_a = mean(Total_Triangles <= 1);		%Probability that at most 1 distinct choice can be made
+
+answer_c_a_X = mean(Total_Triangles);     %Estimated number of potential shipments (X value)
+
+answer_c_a_Y = mean(All_Ratios);        %Estimated ratio (Y value)
+
+answer_d_a_X = std(Total_Triangles);    %Standard deviaton of X
+
+answer_d_a_Y = std(All_Ratios);       %Standard deviation of Y 
+
+display(answer_a);
+display(answer_c_a_X);
+display(answer_c_a_Y);
+display(answer_d_a_X);
+display(answer_d_a_Y);
